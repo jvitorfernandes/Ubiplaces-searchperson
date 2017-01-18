4c5f332a16fad91e85e409eff5f879831a3dff89
João Vítor Fernandes <fernandesjvb@gmail.com>
2017-01-18 19:00:31 -0200

    Puting top searchbox to work and little refactoring

--- js/app.js
+++ js/app.js
@@ -1,39 +1,6 @@
-$("#btn").on("click", function(){
-
-	var searchterm = $(".nome").val();
-    
-    var payload = {"name":searchterm};
-    payload = JSON.stringify(payload);
-    
-
-    
-    $.post("https://dev.api.ubiplaces.com.br/t/searchperson", payload, function(data,status){
-
-	console.log(status); //200
-    
-	list_of_servidores = data.results; //array of objects
-    qt = data.qt;
-    
-        
-    $("#resultados").append('<b>' + qt + ' resultados para "' + searchterm + '":</b>');
-        
-    $('#main-container').hide();
-    $('.header').fadeIn(700);
-    $('#tabela').fadeIn(700);
-        
-        
-    console.log(list_of_servidores)
-
-	for(var i = 0; i < list_of_servidores.length; i++){
-		for(var j in list_of_servidores[i]){
-			console.log(j + ': ' + list_of_servidores[i][j]);
-		}
-		console.log('-------------------------------')
-	}
-    
-    $('.thishead').append('</tbody>');
+function appendToTable(searchterm, list_of_servidores, qt){
+    $(".thishead").empty();
     for(var i = 0; i < list_of_servidores.length; i++){
-        console.log(j + ': ' + list_of_servidores[i][j]);
         $('.thishead').append('<tr>');
         $('.thishead').append('<td>' + list_of_servidores[i].idx + '</td>');
         $('.thishead').append('<td>' + list_of_servidores[i].name + '</td>');
@@ -44,9 +11,53 @@ $("#btn").on("click", function(){
         $('.thishead').append('<td>' + list_of_servidores[i].entity + '</td>');
         $('.thishead').append('</tr>');
 	}
-    $('.thishead').append('</tbody>');
-        
-    },"json");
+    $('#tabela').fadeIn(700);
+}            
 
+function myFunction(searchterm){
+    
+    var payload = {"name":searchterm};
+    payload = JSON.stringify(payload);
+    
+    $.post("https://dev.api.ubiplaces.com.br/t/searchperson", payload, function(data,status){
+
+        console.log(status); //200
+
+        list_of_servidores = data.results; //array of objects
+        qt = data.qt;
+           
+        $("#resultados").empty();
+        $("#resultados").append('<b>' + qt + ' resultados para "' + searchterm + '":</b>');
+        $('.resultado').fadeIn(350);
+        
+        appendToTable(searchterm, list_of_servidores, qt);
+         
+    },"json");
+}
+
+            
+$("#btn").on("click", function(){
+    
+    var searchterm = $(".nome").val();
+    $(".textentry").empty();    
+    console.log(searchterm);
+    console.log(typeof(searchterm));
+    myFunction(searchterm);
+    
+    $('#main-container').hide();
+    $('.header').fadeIn(700);
 });
 
+
+$("#top-btn").on("click", function(){
+    console.log($(".ue").val());
+    var searchterm = $(".ue").val();
+    $(".textentry").empty();
+    
+    console.log(searchterm);
+    console.log(typeof(searchterm));
+    myFunction(searchterm);
+    
+    $('#tabela').fadeOut(700);
+    $('.resultado').fadeOut(700);
+});
